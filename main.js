a0a9180cf8a656d5640405e8efea19741094d1b4
Samuel Oyedeji <32018370+sammyteeoye@users.noreply.github.com>
2022-06-28 16:13:41 +0100

    quiz data injected into html document from API

--- main.js
+++ main.js
@@ -1,17 +1,89 @@
-const login = document.querySelector(".login-page");
-const openQuiz = document.querySelector("#enter-quiz");
-const closeQuiz = document.querySelector("#close-quiz");
-const quiz = document.querySelector(".quiz-container");
-console.log(closeQuiz)
+//declare variables from html document
+const enterQuiz = document.querySelector(".enter-quiz");
+const enterBtn = document.querySelector("#enter-btn");
+const closeBtn = document.querySelector("#close-btn");
+const quizContainer = document.querySelector(".quiz-container");
+const quizForm = document.querySelector("#quiz-form");
+const quizPage = document.querySelector("#quiz-page");
 
-openQuiz.addEventListener("click", function(e){
+
+// declare an empty to store data from form submit for quiz details
+let details = {};
+
+// create function to handle form submit
+function handleSubmit(e) {
     e.preventDefault();
-    quiz.classList.add("active")
-    login.classList.add("hide")
+    const data = new FormData(e.target);
+    const value = Object.fromEntries(data.entries());
+    return value
+}
+
+//submit form data to get type of quiz and fetch data from API
+quizForm.addEventListener("submit", function(e) {
+    details = handleSubmit(e);
+    quizForm.reset();
+    quizContainer.classList.add("active")
+    enterQuiz.classList.add("hide")
+    fetchData();
+
+});
+
+
+closeBtn.addEventListener("click", function(e){
+    e.preventDefault();
+    quizContainer.classList.remove("active")
+    enterQuiz.classList.remove("hide")
 })
 
-closeQuiz.addEventListener("click", function(e){
-    e.preventDefault();
-    quiz.classList.remove("active")
-    login.classList.remove("hide")
-})
+//fetch data from local json file
+//https://opentdb.com/api.php?amount=${details.numberOfQuestions}&category=22&difficulty=${details.difficulty}&type=multiple
+
+
+async function fetchData() {
+    let res = await fetch(`https://opentdb.com/api.php?amount=${details.numberOfQuestions}&category=22&difficulty=${details.difficulty}&type=multiple`);
+    let data = await res.json();
+    data = data.results;
+    console.log(data)
+    createQuiz(data);
+}
+
+//remove quize correct and incorrect answers from data
+function getOptions(data) {
+    let option = [];
+    for (let i = 0; i < data.length; i++) {
+        option.push(data[i].correct_answer);
+        option.push(data[i].incorrect_answers[0]);
+        option.push(data[i].incorrect_answers[1]);
+        option.push(data[i].incorrect_answers[2]);
+    }
+    return sliceIntoChunks(option, 4);
+}
+
+//option into chunks to get options into chunks of arrays
+function sliceIntoChunks(arr, chunkSize) {
+    const res = [];
+    for (let i = 0; i < arr.length; i += chunkSize) {
+        const chunk = arr.slice(i, i + chunkSize);
+        res.push(chunk);
+    }
+    return res;
+}
+
+//create quiz questions and options for answers
+function createQuiz(data) {
+    quizPage.innerHTML = data.map(item => {
+        return `<li class="questions"><p>${item.question}</p></li>`
+    }).join('');
+
+    let options = getOptions(data);
+    console.log(options)
+
+    const questions = Array.from(document.querySelectorAll(".questions"));
+    
+    for(let i = 0; i < questions.length; i++){
+        questions[i].innerHTML += options[i].map(item => {
+            return `<input type="radio" value=${item}>${item}</input>`
+        }).join('');
+    }
+    
+}
